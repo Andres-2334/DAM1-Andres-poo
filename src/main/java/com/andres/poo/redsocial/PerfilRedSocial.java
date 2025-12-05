e98f0335ed070403d07795d262eea87f3d31003e
Andrés Jaime Eduardo Díaz Mercado <777@alumnos.fpsumma.es>
2025-12-05 14:17:58 +0100

    aaa

---
+++ src/main/java/com/andres/poo/redsocial/PerfilRedSocial.java
@@ -0,0 +1,8 @@
+package com.andres.poo.redsocial;
+
+public class PerfilRedSocial {
+String nombreusuario;
+String nombrevisible;
+String biografia;
+String ciudad;
+}
---
+++ src/main/java/com/andres/poo/redsocial/Publicacion.java
@@ -0,0 +1,23 @@
+package com.andres.poo.redsocial;
+
+import java.time.LocalTime;
+
+public class Publicacion {
+	private LocalTime hora;
+	private int numeroLikes; 
+	private String contenido;
+	
+	public void mostrar() {
+		System.out.println(numeroLikes);
+		System.out.println(hora);
+		System.out.println(contenido);
+	}
+	
+	public void enviarLike() {
+		numeroLikes += 1;
+	}
+	
+	public void verMeGusta() {
+		System.out.println(numeroLikes);
+	}
+}
