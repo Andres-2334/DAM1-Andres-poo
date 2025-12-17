dabc14d7ff121f5f6f3ef6d0432d868780c84961
Andrés Jaime Eduardo Díaz Mercado <777@alumnos.fpsumma.es>
2025-12-17 13:30:32 +0100

    main-generador

--- src/main/java/com/andres/poo/redsocial/MainRedSocial.java
+++ src/main/java/com/andres/poo/redsocial/MainRedSocial.java
@@ -1,5 +1,30 @@
 package com.andres.poo.redsocial;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.andres.poo.redsocial.GeneradorPerfiles;
+
+
 public class MainRedSocial {
+	
+	PerfilRedSocial p1 = GeneradorPerfiles.crearPerfilPrueba("juan");
+	PerfilRedSocial p2 = GeneradorPerfiles.crearPerfilPrueba("luis", 3);
+	PerfilRedSocial p3 = GeneradorPerfiles.crearPerfilPrueba("simon");
+	
+	private ArrayList<PerfilRedSocial> perfiles = new ArrayList<>();
+	{
+	perfiles.add(p1);
+	perfiles.add(p2);
+	perfiles.add(p3);
+	perfiles.forEach(perfil -> perfil.mostrarInformacion());
+	perfiles.forEach(perfil -> perfil.mostrarPublicaciones());
+	
+	
+	}
+	
+	
+	
+	
 
 }
